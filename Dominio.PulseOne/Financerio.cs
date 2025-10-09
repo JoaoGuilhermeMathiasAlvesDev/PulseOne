7cec67a39552c96c4d56a5104ceb8d6225860a86
Joao Guilhermem <joaoguilhermeoboista@gmail.com>
2025-10-08 21:17:53 -0300

    Terminando as Entidades

---
+++ Dominio.PulseOne/Financerio.cs
@@ -0,0 +1,39 @@
+﻿using Dominio.PulseOne.Entiteis;
+using Dominio.PulseOne.Entiteis.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.PulseOne
+{
+    public class Financerio : EntityBase
+    {
+        public Guid ProfissionalId { get; private set; }
+        public Profissional Profissional { get; private set; }
+
+        public Double Lucro { get; private set; }
+
+        public double Comissao { get; private set; }
+        public DateTime DataReferente { get; private set; }
+
+        public Financerio(Guid profissionalId, double lucro)
+        {
+            DefinirFinacerio(profissionalId, lucro);
+        }
+
+        public void DefinirFinacerio(Guid profissionalId, double lucro)
+        {
+            if (profissionalId == Guid.Empty)
+                throw new ArgumentNullException("Precisa existir um profissional.");
+
+            ProfissionalId = profissionalId;
+            Lucro = lucro;
+            DataReferente = DateTime.Now;
+        }
+
+    }
+}
