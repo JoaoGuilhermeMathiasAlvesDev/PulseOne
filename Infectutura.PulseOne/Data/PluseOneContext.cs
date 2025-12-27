d1b4c8c9df3f007d2428ffabd2ff7a0aeb8da01d
Joao Guilhermem <joaoguilhermeoboista@gmail.com>
2025-12-27 18:22:59 -0300

    Configuração na context

--- Infectutura.PulseOne/Data/PluseOneContext.cs
+++ Infectutura.PulseOne/Data/PluseOneContext.cs
@@ -24,6 +24,18 @@ namespace Infectutura.PulseOne.Data
         public DbSet<Servico> Servicos { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PluseOneContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
